89cb0804067acca39487d4a8ac281c196c209646
gitzhang <280333911@qq.com>
2019-02-18 16:23:52 +0800

    用户实体类优化字段111

--- src/main/java/com/admin/User.java
+++ src/main/java/com/admin/User.java
@@ -3,10 +3,17 @@ package com.admin;
 import java.util.Date;
 
 public class User {
-    private int id;
-    private String name;
-    private String sex;
+    //id
+    private Long id;
+    //用户名
+    private String username;
+    //性别
+    private Long sex;
     private String age;
     private String address;
+    private Date birth;
+    // 创建时间
     private Date createtime;
+    // 修改时间
+    private Date gmtModified;
 }
